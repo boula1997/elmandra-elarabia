9f15edbbdcf9f6d7c8165c509a4fb6735d37ec6a
boula1997 <boula.nessim.soliman@gmail.com>
2023-10-20 14:43:26 +0300

    making team section dynamic

--- resources/views/front/components/team.blade.php
+++ resources/views/front/components/team.blade.php
@@ -1,8 +1,8 @@
    <!-- Team Start -->
    <div class="container-fluid p-5">
     <div class="mb-5 text-center">
-        <h5 class="text-primary text-uppercase">The Team</h5>
-        <h1 class="display-3 text-uppercase mb-0">Expert Trainers</h1>
+        <h5 class="text-primary text-uppercase">{{ page('team')->title }}</h5>
+        <h1 class="display-3 text-uppercase mb-0">{{ page('team')->subtitle }}s</h1>
     </div>
     <div class="row g-5">
         @foreach ($teams as $team)
