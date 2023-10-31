573f1f98a5d5710cf56d7848afa60b540e8874e9
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2023-10-31 16:14:00 +0200

    aos update

--- resources/views/front/components/testimonial.blade.php
+++ resources/views/front/components/testimonial.blade.php
@@ -1,12 +1,12 @@
         <!-- Testimonial Start -->
         <div class="container-fluid p-0 my-5">
             <div class="row g-0">
-                <div class="col-lg-6" style="min-height: 500px;" data-aos="fade-right">
+                <div class="col-lg-6" style="min-height: 500px;" data-aos="fade-{{ app()->getLocale()=='ar'?'left':'right'}}">
                     <div class="position-relative h-100">
                         <img class="position-absolute w-100 h-100" src="{{ asset(isset(page('testimonial')->images[0]->url) ? page('testimonial')->images[0]->url : asset('default.jpg')) }}" style="object-fit: cover;">
                     </div>
                 </div>
-                <div class="col-lg-6 bg-dark p-5" data-aos="fade-left">
+                <div class="col-lg-6 bg-dark p-5" data-aos="fade-{{ app()->getLocale()=='ar'?'right':'left'}}">
                     <div class="mb-5">
                         <h5 class="text-primary text-uppercase">{{ page('testimonial')->title }}</h5>
                         <h1 class="display-5 text-uppercase text-light mb-0">{{ page('testimonial')->subtitle }}</h1>
