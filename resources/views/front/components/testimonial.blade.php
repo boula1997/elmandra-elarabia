cbe7330c99b24e4828ca37844320876862d58fa2
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2023-10-31 16:05:04 +0200

    update

--- resources/views/front/components/testimonial.blade.php
+++ resources/views/front/components/testimonial.blade.php
@@ -24,7 +24,7 @@
                             </div>
                         </div> 
                         @endforeach
-                       
+                       {{-- new --}}
                         {{-- <div class="testimonial-item">
                             <p class="fs-4 fw-normal text-light mb-4"><i class="fa fa-quote-left text-primary me-3"></i>Dolores sed duo clita tempor justo dolor et stet lorem kasd labore dolore lorem ipsum. At lorem lorem magna ut et, nonumy et labore et tempor diam tempor erat dolor rebum sit ipsum.</p>
                             <div class="d-flex align-items-center">
