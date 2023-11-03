b89d2b5dd5f10d970954d23f867fbbf2de0651e9
boula1997 <boula.nessim.soliman@gmail.com>
2023-11-03 19:26:10 +0200

    edit profile

--- resources/views/front/components/profile.blade.php
+++ resources/views/front/components/profile.blade.php
@@ -2,7 +2,9 @@
 <div class="container rounded bg-white mt-5 mb-5">
     <div class="row ">
         <div class="col-md-3 border-right">
-            <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold">{{ Auth::user()->name }}</span><span class="text-black-50">{{ Auth::user()->email }}</span><span> </span></div>
+            <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+                <img class="rounded-circle mt-5" width="100px" height="100px" src="{{ Auth::user()->image }}">
+                <span class="font-weight-bold">{{ Auth::user()->name }}</span><span class="text-black-50">{{ Auth::user()->email }}</span><span> </span></div>
         </div>
         <div class="col-md-5 border-right">
             <div class="p-3 py-5">
@@ -10,15 +12,15 @@
                     <h4 class="text-right">{{ __('general.profile_settings') }}</h4>
                 </div>
               
-                <form  method="post"  action="{{ route('update_profile',Auth::user()->id) }}">
+                <form  method="post"  action="{{ route('update_profile',Auth::user()->id) }}" enctype="multipart/form-data">
                     @csrf
                     <div class="row mt-2">
-                        <div class="col-md-12"><label class="labels">{{ __('general.name') }}</label><input type="text" class="form-control @error('name') invalid @enderror" name="name" placeholder="{{ Auth::user()->name }}" value="{{ Auth::user()->name }}" />
+                        <div class="col-md-12"><label class="labels">{{ __('general.name') }}</label><input type="text" class="form-control @error('name') invalid @enderror" name="name" placeholder="{{ Auth::user()->name }}" value="{{ old('name',Auth::user()->name) }}" />
                             @error('name')
                             <span class="text-danger">{{ $message }}</span>
                             @enderror
                         </div>
-                        <div class="col-md-12"><label class="labels">{{ __('general.email') }}</label><input type="text" class="form-control @error('name') invalid @enderror" name="email" placeholder="{{ Auth::user()->email }}" value="{{ Auth::user()->email }}"/>
+                        <div class="col-md-12"><label class="labels">{{ __('general.email') }}</label><input type="text" class="form-control @error('email') invalid @enderror" name="email" placeholder="{{ Auth::user()->email }}" value="{{ old('email',Auth::user()->email) }}"/>
                             @error('email')
                             <span class="text-danger">{{ $message }}</span>
                             @enderror
@@ -31,8 +33,13 @@
                             @enderror
                         </div>
 
-                        <div class="col-md-12"><label class="labels">{{ __('general.confirm_password') }}</label><input type="password" class="form-control @error('name') invalid @enderror" name="confirm_password" placeholder="{{ __('general.confirm_password') }}"/>
-                            @error('confirm_password')
+                        <div class="col-md-12"><label class="labels">{{ __('general.confirm_password') }}</label><input type="password" class="form-control @error('password_confirmation') invalid @enderror" name="password_confirmation" placeholder="{{ __('general.confirm_password') }}"/>
+                            @error('password_confirmation')
+                            <span class="text-danger">{{ $message }}</span>
+                            @enderror   
+                        </div>
+                        <div class="col-md-12 pt-2"><label class="labels">{{ __('general.image') }}</label><input type="file" class="form-control @error('image') invalid @enderror" name="image" placeholder="{{ __('general.image') }}"/>
+                            @error('image')
                             <span class="text-danger">{{ $message }}</span>
                             @enderror
                         </div>
