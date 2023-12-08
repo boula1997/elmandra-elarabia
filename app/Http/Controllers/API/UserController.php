222de13606ffed43b8828b1d41026a73baf7171a
boula1997 <boula.nessim.soliman@gmail.com>
2023-12-08 18:11:54 +0200

    commenting email until created on server

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\UserRequest;
+use App\Http\Resources\UserResource;
 use App\Mail\VerifyAdminMai;
 use App\Mail\VerifyUserMail;
 use App\Models\User;
@@ -17,9 +18,10 @@ class UserController extends Controller
         try {
             $inputs=$request->all();
             $inputs['seller_id']=auth('api')->user()->id;
-            $data = User::create($inputs);
-            $data->uploadFile();
-            // Mail::to(env('MAIL_FROM_NAME'))->send(new VerifyUserMail($data));
+            $user = User::create($inputs);
+            $user->uploadFile();
+            // Mail::to(env('MAIL_FROM_NAME'))->send(new VerifyUserMail($user));
+            $data['user'] = new UserResource($user);
             return successResponse($data);
         } catch (Exception $e) {
             return failedResponse($e->getMessage());
