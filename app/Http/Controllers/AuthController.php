2283e2e75d64149844f08fe064d0b1f0448e0966
boula1997 <boula.nessim.soliman@gmail.com>
2023-12-10 19:37:44 +0200

    editting api location

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -47,6 +47,7 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|between:2,100',
             'email' => 'required|string|email|max:100|unique:users',
+            'phone' => 'nullable|string|email|max:100|unique:users',
             'password' => 'required|string|confirmed|min:6',
         ]);
         if($validator->fails()){
