4c4c9cc5dbb5f20ac99a46c6a71f5d3ac4683559
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2023-12-10 16:47:16 +0200

    adding offers

--- resources/views/front/components/offers.blade.php
+++ resources/views/front/components/offers.blade.php
@@ -2,7 +2,7 @@
     <section class="bank-section overflow-hidden">
         <div class="container-fluid-lg">
             <div class="title">
-                <h2>Bank & Wallet Offers</h2>
+                <h2>{{__('general.offers')}}</h2>
             </div>
             <div class="slider-bank-3 arrow-slider slick-height">
                 @foreach($offers as $offer)
