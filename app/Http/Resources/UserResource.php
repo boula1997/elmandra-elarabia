0153249ab3fc64fb1dbc20122e5f7fb7f0aff76a
boula1997 <boula.nessim.soliman@gmail.com>
2023-12-10 19:25:37 +0200

    editting api location

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,7 +14,6 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {  
-        dd(10);
         return [
             "id" => $this->id,
             "image" => $this->image,
