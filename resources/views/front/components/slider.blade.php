5541e438882847fabb767c6c88ce7414a96a1141
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2023-12-13 20:16:35 +0200

    updating slider content and layout

--- resources/views/front/components/slider.blade.php
+++ resources/views/front/components/slider.blade.php
@@ -1,44 +1,32 @@
-      <!-- Home Section Start -->
-      <section class="home-section pt-2">
-        <div class="container-fluid-lg">
-            <div class="row g-4">
-                <div class="col-xl-9 col-lg-8 float-right">
-                    <div class="home-contain h-100">
-                        <img src="{{ asset(isset(page('slider')->images[0]->url) ? page('slider')->images[0]->url : asset('default.jpg')) }}" class="bg-img blur-up lazyload" alt="">
-                        <div class="home-detail p-center-left w-75 position-relative mend-auto">
-                            <div>
-                                <h6>{{$advertisements[0]->translate(app()->getLocale())->subtitle}}<span>{{$advertisements[0]->title}}</span></h6>
-                                <h1 class="w-100 text-uppercase poster-1">{{ page('slider')->title }} <span
-                                        class="daily">{{ page('slider')->subtitle }}</span></h1>
-                                <p class="w-100 d-none d-sm-block">{!! page('slider')->description !!}</p>
-                                <button onclick="location.href = '{{ route('front.show-products') }}';"
-                                    class="btn btn-animation mt-xxl-4 mt-2 home-button mend-auto">{{ __('general.shop_now') }}<i
-                                        class="fa-solid fa-right-long ms-2 icon"></i></button>
+<section class="banner-section p-0 m-0">
+    <div class="">
+        <div class="">
+            <div class="swiper mySwiper" data-aos="zoom-in">
+                <div class="swiper-wrapper">
+                    @foreach ( $sliders as $slider )      
+                        <div class="swiper-slide bg-success">
+                            <img class="w-100 img-slider" style="height: 75vh;"
+                            src="{{ asset($slider->image) }}" alt="Image">
+                            
+                            <div
+                            class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                            <div class="p-3">
+                                    <h5 class="text-dark text-uppercase mb-3 animated slideInDown">{{ $slider->title }}
+                                    </h5>
+                                    <h1 class="display-4 text-dark mb-md-4 animated zoomIn">{{ $slider->subtitle }}
+                                    </h1>
+                                    
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
-
-                <div class="col-xl-3 col-lg-4 d-lg-inline-block d-none ratio_156">
-                    <div class="home-contain h-100">
-                        <img src="{{ asset(isset(page('mini_slider')->images[0]->url) ? page('mini_slider')->images[0]->url : asset('default.jpg')) }}" class="bg-img blur-up lazyload" alt="">
-                        <div class="home-detail p-top-left home-p-sm w-75">
-                            <div>
-                                <h2 class="mt-0 text-danger">{{$advertisements[1]->translate(app()->getLocale())->subtitle}} <span class="discount text-title">{{$advertisements[1]->title}}</span>
-                                </h2>
-                                <h3 class="w-75 text-uppercase poster-1">{{ page('mini_slider')->title }}</h3>
-                                <h3 class="theme-color">{{ page('mini_slider')->subtitle }}</h3>
-                                <h5 class="text-content">{!! page('mini_slider')->description !!}</h5>
-                                <a href="{{ route('front.show-products') }}" class="shop-button">{{ __('general.shop_now') }}<i
-                                        class="fa-solid fa-right-long ms-2"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <div class="swiper-pagination"></div>
             </div>
         </div>
-    </section>
-    <!-- Home Section End -->
-    
+
+    </div>
+</section>
+
     
    
