b83ace8119cf7ff5d912978ba6716de2ddc85dd3
boula1997 <boula.nessim.soliman@gmail.com>
2024-01-19 18:20:29 +0200

    completing the filter

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -46,20 +46,46 @@ class Product extends Model implements TranslatableContract
            return $q->whereHas('translations', function ($q2) {
                $q2->where('title', 'like', '%' . request()->title . '%');
            });
-       })->when(request()->price, function ($q) {
-           return $q->where('price', request()->price);
-       })->when(request()->year, function ($q) {
-           return $q->whereYear('start_date', request()->year);
-       })->when(request()->city_id, function ($q) {
-           return $q->where('city_id', request()->city_id);
-       })->when(request()->section_id, function ($q) {
-           return $q->where('section_id', request()->section_id);
-       })->when(request()->category_id, function ($q) {
-        // dd( $q->where('category_id', request()->category_id)->get());
-           return $q->where('category_id', request()->category_id);
-       });
+        })->when(request()->price, function ($q) {
+            return $q->where('price', request()->price);
+        })->when(request()->year, function ($q) {
+            return $q->whereYear('start_date', request()->year);
+        })->when(request()->city_id, function ($q) {
+            return $q->where('city_id', request()->city_id);
+        })->when(request()->section_id, function ($q) {
+            return $q->where('section_id', request()->section_id);
+        })->when(request()->category_id, function ($q) {
+            return $q->where('category_id', request()->category_id);
+        })->when(request()->kind, function ($q) {
+            return $q->where('kind', request()->kind);
+        })->when(request()->suspensionType, function ($q) {
+            return $q->where('suspensionType', request()->suspensionType);
+        })->when(request()->axelsNumber, function ($q) {
+            return $q->where('axelsNumber', request()->axelsNumber);
+        })->when(request()->gearBox, function ($q) {
+            return $q->where('gearBox', request()->gearBox);
+        })->when(request()->configuration, function ($q) {
+            return $q->where('configuration', request()->configuration);
+        })->when(request()->engineCapacity, function ($q) {
+            return $q->where('engineCapacity', request()->engineCapacity);
+        })->when(request()->mileAge, function ($q) {
+            return $q->where('mileAge', request()->mileAge);
+        })->when(request()->number, function ($q) {
+            return $q->where('number', request()->number);
+        })->when(request()->manYear, function ($q) {
+            return $q->where('manYear', request()->manYear);
+        })->when(request()->promotions, function ($q) {
+            return $q->where('promotions', request()->promotions);
+        })->when(request()->emission_code, function ($q) {
+            return $q->where('emission_code', request()->emission_code);
+        })->when(request()->fuel_type, function ($q) {
+            return $q->where('fuel_type', request()->fuel_type);
+        })->when(request()->damaged, function ($q) {
+            return $q->where('damaged', request()->damaged);
+        })->when(request()->dutch_trucks, function ($q) {
+            return $q->where('dutch_trucks', request()->dutch_trucks);
+        });
    }
 
     
 }
-
