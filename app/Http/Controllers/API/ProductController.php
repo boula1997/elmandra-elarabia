6b56c95fbefabc7bc21f8cc9b88aea586e07c207
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2025-04-14 18:31:22 +0200

    update

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -31,10 +31,10 @@ class ProductController extends Controller
             return failedResponse($e->getMessage());
         }
     }
-    public function store()
+    public function store(Request $request)
     {
         try {
-      dd(request()->kind);
+      dd($request->kind);
             $data['trailersCranes_section'] = new PageResource(page('trailersCranes'));
             $data['spareParts_section'] = new PageResource(page('spareParts'));
             $data['tractorUnits_section'] = new PageResource(page('tractorUnits'));
