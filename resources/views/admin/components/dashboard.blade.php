93a4d7c54371e3699a439077fe7c50eeaf20401a
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2025-06-16 22:49:06 +0300

    update

--- resources/views/admin/components/dashboard.blade.php
+++ resources/views/admin/components/dashboard.blade.php
@@ -226,6 +226,27 @@
                     </li>
                 @endcan
 
+
+                @can('subcategory-list')
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="fab fa-buromobelexperte"></i>
+                            <p>
+                                @lang('general.subcategories') <i class=" px-1 fas fa-angle-left right"></i>
+                                <span class="badge badge-info right">{{ itemsCount('subcategories') }}</span>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ route('subcategories.index') }}" class="nav-link">
+                                    <i class=" px-1 far fa-circle nav-icon"></i>
+                                    <p>@lang('general.show')</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                @endcan
+
                 @can('company-list')
                     <li class="nav-item">
                         <a href="#" class="nav-link">
